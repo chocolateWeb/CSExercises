88647f4138867c5b1203eb28ec38fb2478d13398
chowkeljinA0137848N <e0001364@u.nus.edu>
2015-08-17 13:53:51 +0800

    Complete Exercise 01

--- CSExercises/Ex01-06/Ex01.cs
+++ CSExercises/Ex01-06/Ex01.cs
@@ -10,7 +10,10 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            string name;
+           name = Console.ReadLine();
+
+            Console.WriteLine("Good morning " + name);
         }
     }
 }
